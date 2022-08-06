5209c0c29895043698893a43336398b6c7ece18d
jaykv <improcuziam@gmail.com>
2022-08-06 19:40:56 -0400

    Optional mypy

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,55 +12,33 @@ shopt -s globstar
 
 echo "===== Running Python Linter - '$2' ====="
 
-echo "Running mypy"
-mypy "$1" --show-error-codes --show-error-context --pretty ${3}
+if [ "$3" != false ]; then
+  echo "Running mypy"
+  mypy "$1" --show-error-codes --show-error-context --pretty ${3}
+fi
 
 echo "Running black"
-if [ "$7" ]; then
-  if [ "$2" = "high" ]; then
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" ${4}
+if [ "$2" = "high" ]; then
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
+  echo "Running pylint"
+  pylint "$1" ${4}
 
-  elif [ "$2" = "medium" ]; then
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" --fail-under=8 ${4}
-
-  else
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
-    echo "Running pylint"
-    pylint "$1" --exit-zero ${4}
-  fi
+elif [ "$2" = "medium" ]; then
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
+  echo "Running pylint"
+  pylint "$1" --fail-under=8 ${4}
 
 else
-  if [ "$2" = "high" ]; then
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" ${4}
-
-  elif [ "$2" = "medium" ]; then
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" --fail-under=8 ${4}
-
-  else
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
-    echo "Running pylint"
-    pylint "$1" --exit-zero ${4}
-  fi
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
+  echo "Running pylint"
+  pylint "$1" --exit-zero ${4}
 fi
+
 
 echo "Done 🎉" ; echo ""
